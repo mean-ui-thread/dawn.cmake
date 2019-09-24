ebca86c732f86638d9329f029aba718b4893d113
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-09-24 19:23:03 -0400

    improved source packaging script

--- package_source.sh
+++ package_source.sh
@@ -4,5 +4,5 @@ current_dir=$(pwd)
 script_dir=$(dirname "$0")
 
 pushd ${script_dir}
-tar czvf ${current_dir}/source.tar.gz --exclude=**/.git --exclude=**/*.pyc --exclude="source.tar.gz" .
+tar czvf ${current_dir}/source.tar.gz --exclude="**/.git*" --exclude="**/CMakeLists.txt.user" --exclude=**/*.pyc --exclude="**/source.tar.gz" --exclude="**/package_source.sh" *
 popd
