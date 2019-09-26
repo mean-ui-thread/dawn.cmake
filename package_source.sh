0381722c53d7d126c3ffb62b9c3f2cb48df33853
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-09-26 16:53:45 -0400

    convenient create_release shell script

--- package_source.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-current_dir=$(pwd)
-script_dir=$(dirname "$0")
-
-pushd ${script_dir}
-tar czvf ${current_dir}/source.tar.gz --exclude="**/.git*" --exclude="**/CMakeLists.txt.user" --exclude=**/*.pyc --exclude="**/source.tar.gz" --exclude="**/package_source.sh" *
-popd
